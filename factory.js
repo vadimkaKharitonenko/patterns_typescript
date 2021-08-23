a8e7173ec40a5a2639f5507676e2f1933f3a6451
Вадим Харитоненко <vadimka.kharitonenko@mail.ru>
2021-08-23 10:41:17 +0700

    Added example for factory method.

--- factory.js
+++ factory.js
@@ -1,57 +1,193 @@
-var CheesePizza = /** @class */ (function () {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Pizza = /** @class */ (function () {
+    function Pizza() {
+        this.toppings = [];
+    }
+    Pizza.prototype.prepare = function () {
+        console.log('Preparing ' + this.name);
+        console.log('Tossing dough...');
+        console.log('Adding souce...');
+        console.log('Adding toppings: ');
+        for (var i = 0; i < this.toppings.length; i++) {
+            console.log('  ' + this.toppings[i]);
+        }
+    };
+    ;
+    Pizza.prototype.bake = function () {
+        console.log('Bake for 25 minutes at 350');
+    };
+    ;
+    Pizza.prototype.cut = function () {
+        console.log('Cutting the pizza into diagonal slices');
+    };
+    ;
+    Pizza.prototype.box = function () {
+        console.log('Place pizza in official PizzaStore box');
+    };
+    ;
+    Pizza.prototype.getName = function () {
+        return this.name;
+    };
+    return Pizza;
+}());
+var PizzaStore = /** @class */ (function () {
+    function PizzaStore() {
+    }
+    PizzaStore.prototype.orderPizza = function (type) {
+        var pizza;
+        pizza = this.createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    };
+    return PizzaStore;
+}());
+var NYStyleStore = /** @class */ (function (_super) {
+    __extends(NYStyleStore, _super);
+    function NYStyleStore() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    NYStyleStore.prototype.createPizza = function (type) {
+        var pizza;
+        if (type === 'cheese') {
+            pizza = new NYStyleCheesePizza();
+        }
+        else if (type === 'pepperoni') {
+            pizza = new NYStylePepperoniPizza();
+        }
+        else if (type === 'clam') {
+            pizza = new NYStyleClamPizza();
+        }
+        return pizza;
+    };
+    return NYStyleStore;
+}(PizzaStore));
+var ChicagoStyleStore = /** @class */ (function (_super) {
+    __extends(ChicagoStyleStore, _super);
+    function ChicagoStyleStore() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ChicagoStyleStore.prototype.createPizza = function (type) {
+        var pizza;
+        if (type === 'cheese') {
+            pizza = new ChicagoStyleCheesePizza();
+        }
+        else if (type === 'pepperoni') {
+            pizza = new ChicagoStylePepperoniPizza();
+        }
+        else if (type === 'clam') {
+            pizza = new ChicagoStyleClamPizza();
+        }
+        return pizza;
+    };
+    return ChicagoStyleStore;
+}(PizzaStore));
+var CheesePizza = /** @class */ (function (_super) {
+    __extends(CheesePizza, _super);
     function CheesePizza() {
-        this.name = 'CheesePizza';
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'CheesePizza';
+        return _this;
     }
-    CheesePizza.prototype.prepare = function () {
-        console.log(this.name + " - is prepared!");
-    };
-    CheesePizza.prototype.bake = function () {
-        console.log(this.name + " - is baked!");
-    };
-    CheesePizza.prototype.cut = function () {
-        console.log(this.name + " - is cut!");
-    };
-    CheesePizza.prototype.box = function () {
-        console.log(this.name + " - is boxed!");
-    };
     return CheesePizza;
-}());
-var PepperoniPizza = /** @class */ (function () {
+}(Pizza));
+var PepperoniPizza = /** @class */ (function (_super) {
+    __extends(PepperoniPizza, _super);
     function PepperoniPizza() {
-        this.name = 'PepperoniPizza';
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'PepperoniPizza';
+        return _this;
     }
-    PepperoniPizza.prototype.prepare = function () {
-        console.log(this.name + " - is prepared!");
-    };
-    PepperoniPizza.prototype.bake = function () {
-        console.log(this.name + " - is baked!");
-    };
-    PepperoniPizza.prototype.cut = function () {
-        console.log(this.name + " - is cut!");
-    };
-    PepperoniPizza.prototype.box = function () {
-        console.log(this.name + " - is boxed!");
-    };
     return PepperoniPizza;
-}());
-var ClamPizza = /** @class */ (function () {
+}(Pizza));
+var ClamPizza = /** @class */ (function (_super) {
+    __extends(ClamPizza, _super);
     function ClamPizza() {
-        this.name = 'ClamPizza';
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'ClamPizza';
+        return _this;
     }
-    ClamPizza.prototype.prepare = function () {
-        console.log(this.name + " - is prepared!");
-    };
-    ClamPizza.prototype.bake = function () {
-        console.log(this.name + " - is baked!");
-    };
-    ClamPizza.prototype.cut = function () {
-        console.log(this.name + " - is cut!");
-    };
-    ClamPizza.prototype.box = function () {
-        console.log(this.name + " - is boxed!");
-    };
     return ClamPizza;
-}());
+}(Pizza));
+var NYStyleCheesePizza = /** @class */ (function (_super) {
+    __extends(NYStyleCheesePizza, _super);
+    function NYStyleCheesePizza() {
+        var _this = _super.call(this) || this;
+        _this.name = 'NYStyleCheesePizza';
+        _this.dough = 'Thin Crust Dough';
+        _this.sauce = 'Marinara Sauce';
+        _this.toppings.push('Grated Reggiano Cheese');
+        return _this;
+    }
+    return NYStyleCheesePizza;
+}(Pizza));
+var NYStylePepperoniPizza = /** @class */ (function (_super) {
+    __extends(NYStylePepperoniPizza, _super);
+    function NYStylePepperoniPizza() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'NYStylePepperoniPizza';
+        return _this;
+    }
+    return NYStylePepperoniPizza;
+}(Pizza));
+var NYStyleClamPizza = /** @class */ (function (_super) {
+    __extends(NYStyleClamPizza, _super);
+    function NYStyleClamPizza() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'NYStyleClamPizza';
+        return _this;
+    }
+    return NYStyleClamPizza;
+}(Pizza));
+var ChicagoStyleCheesePizza = /** @class */ (function (_super) {
+    __extends(ChicagoStyleCheesePizza, _super);
+    function ChicagoStyleCheesePizza() {
+        var _this = _super.call(this) || this;
+        _this.name = 'ChicagoStyleCheesePizza';
+        _this.dough = 'Extra Thick Crust Dough';
+        _this.sauce = 'Plum Tomato Sauce';
+        _this.toppings.push('Sgredded Mozzarella Cheese');
+        return _this;
+    }
+    ChicagoStyleCheesePizza.prototype.cut = function () {
+        console.log('Cutting the pizza into square slices');
+    };
+    return ChicagoStyleCheesePizza;
+}(Pizza));
+var ChicagoStylePepperoniPizza = /** @class */ (function (_super) {
+    __extends(ChicagoStylePepperoniPizza, _super);
+    function ChicagoStylePepperoniPizza() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'ChicagoStylePepperoniPizza';
+        return _this;
+    }
+    return ChicagoStylePepperoniPizza;
+}(Pizza));
+var ChicagoStyleClamPizza = /** @class */ (function (_super) {
+    __extends(ChicagoStyleClamPizza, _super);
+    function ChicagoStyleClamPizza() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.name = 'ChicagoStyleClamPizza';
+        return _this;
+    }
+    return ChicagoStyleClamPizza;
+}(Pizza));
 var SimplePizzaFactory = /** @class */ (function () {
     function SimplePizzaFactory() {
     }
@@ -70,25 +206,12 @@ var SimplePizzaFactory = /** @class */ (function () {
     };
     return SimplePizzaFactory;
 }());
-var PizzaStore = /** @class */ (function () {
-    function PizzaStore(factory) {
-        this.factory = factory;
-    }
-    PizzaStore.prototype.orderPizza = function (type) {
-        var pizza;
-        pizza = this.factory.createPizza(type);
-        pizza.prepare();
-        pizza.bake();
-        pizza.cut();
-        pizza.box();
-        return pizza;
-    };
-    return PizzaStore;
-}());
 function main() {
-    var factory = new SimplePizzaFactory();
-    var store = new PizzaStore(factory);
-    store.orderPizza('pepperoni');
-    store.orderPizza('cheese');
+    var nyStore = new NYStyleStore();
+    var chicagoStore = new ChicagoStyleStore();
+    var pizza = nyStore.orderPizza('cheese');
+    console.log('Ethan ordered a ' + pizza.getName());
+    pizza = chicagoStore.orderPizza('cheese');
+    console.log('Joel ordered a ' + pizza.getName());
 }
 main();
--- factory.ts
+++ factory.ts
@@ -1,56 +1,130 @@
 abstract class Pizza {
   name: string;
-  public prepare(): void {};
-  public bake(): void {};
-  public cut(): void {};
-  public box(): void {};
+  dough: string;
+  sauce: string;
+  toppings: Array<string> = [];
+
+  public prepare(): void {
+    console.log('Preparing ' + this.name);
+    console.log('Tossing dough...');
+    console.log('Adding souce...');
+    console.log('Adding toppings: ');
+    for(let i = 0; i < this.toppings.length; i++) {
+      console.log('  ' + this.toppings[i]);
+    }
+  };
+  public bake(): void {
+    console.log('Bake for 25 minutes at 350');
+  };
+  public cut(): void {
+    console.log('Cutting the pizza into diagonal slices');
+  };
+  public box(): void {
+    console.log('Place pizza in official PizzaStore box');
+  };
+  public getName(): string {
+    return this.name;
+  }
+}
+
+abstract class PizzaStore {
+  public orderPizza(type: string): Pizza {
+    let pizza: Pizza;
+
+    pizza = this.createPizza(type);
+    pizza.prepare();
+    pizza.bake();
+    pizza.cut();
+    pizza.box();
+
+    return pizza;
+  }
+
+  abstract createPizza(type: string): Pizza;
+}
+
+class NYStyleStore extends PizzaStore {
+  createPizza(type: string): Pizza {
+    let pizza: Pizza;
+
+    if (type === 'cheese') {
+      pizza = new NYStyleCheesePizza();
+    } else if (type === 'pepperoni') {
+      pizza = new NYStylePepperoniPizza();
+    } else if (type === 'clam') {
+      pizza = new NYStyleClamPizza();
+    }
+
+    return pizza;
+  }
+}
+
+class ChicagoStyleStore extends PizzaStore {
+  createPizza(type: string): Pizza {
+    let pizza: Pizza;
+
+    if (type === 'cheese') {
+      pizza = new ChicagoStyleCheesePizza();
+    } else if (type === 'pepperoni') {
+      pizza = new ChicagoStylePepperoniPizza();
+    } else if (type === 'clam') {
+      pizza = new ChicagoStyleClamPizza();
+    }
+
+    return pizza;
+  }
 }
 
 class CheesePizza extends Pizza {
   name = 'CheesePizza';
-  public prepare(): void {
-    console.log(`${this.name} - is prepared!`);
-  }
-  public bake(): void {
-    console.log(`${this.name} - is baked!`);
-  }
-  public cut(): void {
-    console.log(`${this.name} - is cut!`);
-  }
-  public box(): void {
-    console.log(`${this.name} - is boxed!`);
-  }
 }
 
 class PepperoniPizza extends Pizza {
   name = 'PepperoniPizza';
-  public prepare(): void {
-    console.log(`${this.name} - is prepared!`);
-  }
-  public bake(): void {
-    console.log(`${this.name} - is baked!`);
-  }
-  public cut(): void {
-    console.log(`${this.name} - is cut!`);
-  }
-  public box(): void {
-    console.log(`${this.name} - is boxed!`);
-  }
 }
 class ClamPizza extends Pizza {
   name = 'ClamPizza';
-  public prepare(): void {
-    console.log(`${this.name} - is prepared!`);
+}
+
+class NYStyleCheesePizza extends Pizza {
+  name = 'NYStyleCheesePizza';
+  dough = 'Thin Crust Dough';
+  sauce = 'Marinara Sauce';
+
+  constructor() {
+    super();
+    this.toppings.push('Grated Reggiano Cheese');
   }
-  public bake(): void {
-    console.log(`${this.name} - is baked!`);
+}
+
+class NYStylePepperoniPizza extends Pizza {
+  name = 'NYStylePepperoniPizza';
+}
+class NYStyleClamPizza extends Pizza {
+  name = 'NYStyleClamPizza';
+}
+
+class ChicagoStyleCheesePizza extends Pizza {
+  name = 'ChicagoStyleCheesePizza';
+  dough = 'Extra Thick Crust Dough';
+  sauce = 'Plum Tomato Sauce';
+
+  constructor() {
+    super();
+
+    this.toppings.push('Sgredded Mozzarella Cheese');
   }
-  public cut(): void {
-    console.log(`${this.name} - is cut!`);
+
+  cut(): void {
+    console.log('Cutting the pizza into square slices');
   }
-  public box(): void {
-    console.log(`${this.name} - is boxed!`);
-  }
+}
+
+class ChicagoStylePepperoniPizza extends Pizza {
+  name = 'ChicagoStylePepperoniPizza';
+}
+class ChicagoStyleClamPizza extends Pizza {
+  name = 'ChicagoStyleClamPizza';
 }
 
 class SimplePizzaFactory {
@@ -69,31 +143,17 @@ class SimplePizzaFactory {
   }
 }
 
-class PizzaStore {
-  factory: SimplePizzaFactory;
 
-  constructor(factory: SimplePizzaFactory) {
-    this.factory = factory;
-  }
-
-  public orderPizza(type: string) {
-    let pizza: Pizza;
-
-    pizza = this.factory.createPizza(type);
-    pizza.prepare();
-    pizza.bake();
-    pizza.cut();
-    pizza.box();
-
-    return pizza;
-  }
-}
 
 function main() {
-  const factory = new SimplePizzaFactory();
-  const store = new PizzaStore(factory);
-  store.orderPizza('pepperoni');
-  store.orderPizza('cheese');
+  const nyStore = new NYStyleStore();
+  const chicagoStore = new ChicagoStyleStore();
+
+  let pizza: Pizza = nyStore.orderPizza('cheese');
+  console.log('Ethan ordered a ' + pizza.getName());
+
+  pizza = chicagoStore.orderPizza('cheese');
+  console.log('Joel ordered a ' + pizza.getName());
 }
 
 main();
